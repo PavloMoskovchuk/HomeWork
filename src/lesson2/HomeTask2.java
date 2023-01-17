550f4dc416055489fc70746dc73a8b10155c5363
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-01-17 13:13:15 +0200

    task 2

---
+++ src/lesson2/HomeTask2.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class HomeTask2 {
+    public static void main(String[] args) {
+        System.out.println("Helo World!!!");
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+    }
+}
