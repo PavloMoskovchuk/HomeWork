181598683e99a1f209a98662ec08fa303d7a8e98
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-01-19 20:11:03 +0200

    task 4

---
+++ src/lesson4/main.java
@@ -0,0 +1,9 @@
+package lesson4;
+
+public class main {
+    public static void main(String[] args) {
+        // String and operations with them
+        String str = "cgvhb";
+        System.out.println(str);
+    }
+}
