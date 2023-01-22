c1e2449f68307d819f40d6894478327a3598c9a7
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-01-22 16:47:01 +0200

    add lesson5

---
+++ src/lesson5/HomeWork.java
@@ -0,0 +1,7 @@
+package lesson5;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lesson5/main.java
@@ -0,0 +1,7 @@
+package lesson5;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
