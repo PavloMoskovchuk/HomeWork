6a787a4f4f28ffc87b9948ea67729c907335565d
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-01-24 13:58:17 +0200

    Home task for lesson 5 done

--- src/lesson5/HomeWork.java
+++ src/lesson5/HomeWork.java
@@ -1,7 +1,33 @@
 package lesson5;
+import java.util.Scanner;
 
 public class HomeWork {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int max = 0;
+        int min = 0;
+        if (a > b) {
+            max = a;
+            min = b;
+            System.out.println("Min number is " + min);
+            System.out.println("Max number is " + max);
+        } else if (a < b) {
+            max = b;
+            min = a;
+            System.out.println("Min number is " + min);
+            System.out.println("Max number is " + max);
+        } else {
+            System.out.println("a equals b");
+        }
+        if (a%2 == 0 ) {
+            System.out.println("Number a is even");
+        }
+        else {
+            System.out.println("Number a is odd");
+        }
+        sc.close();
     }
 }
