c5a9199136214630052797fa414224e33a267b45
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-02-24 13:20:03 +0200

    Home Work 12 Edition1

---
+++ src/lesson13/practice/statictic/StudentStatistic.java
@@ -0,0 +1,5 @@
+package lesson13.practice.statictic;
+
+public class StudentStatistic {
+
+}
---
+++ src/lesson14/Main.java
@@ -0,0 +1,7 @@
+package lesson14;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
