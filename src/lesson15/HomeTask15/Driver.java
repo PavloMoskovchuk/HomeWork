161b9f698250e4f59f8d7b0c2629e3a1dece3a2c
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-03-02 11:51:37 +0200

    Home Work 15

--- src/lesson15/HomeTask15/Driver.java
+++ src/lesson15/HomeTask15/Driver.java
@@ -1,11 +1,11 @@
 package lesson15.HomeTask15;
 
 public class Driver extends Human  {
-    @Override
+
     public void drive() {
         System.out.println("Driver is driving");
     }
-    @Override
+
     public void fly() {
         System.out.println("Driver can fly after studying");
     }
--- src/lesson15/HomeTask15/Human.java
+++ src/lesson15/HomeTask15/Human.java
@@ -2,12 +2,12 @@ package lesson15.HomeTask15;
 
 public class Human implements DrivingProcess, FlyingProcess {
 
-    @Override
+
     public void drive() {
         System.out.println("Human can drive");
     }
 
-    @Override
+
     public void fly() {
         System.out.println("Human can fly");
     }
