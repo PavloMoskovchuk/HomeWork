d9af2102bbdb2914f4e0ab5fb891db80166900ed
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-04-03 14:32:43 +0300

    23 task

--- src/lesson22/Main.java
+++ src/lesson22/Main.java
@@ -1,9 +1,6 @@
 package lesson22;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
