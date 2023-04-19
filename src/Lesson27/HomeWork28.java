c74b67dfca164be7ed326e1eacaec8775ee886e6
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-04-19 14:04:59 +0300

    28 task

--- src/Lesson27/HomeWork28.java
+++ src/Lesson27/HomeWork28.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class HomeWork28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = "";
-        Path path = Path.of("1.txt");
+        Path path = Path.of("D:\\раб стол\\progs\\add.txt");
         try {
             Files.createFile(path);
         } catch (IOException e) {
@@ -20,8 +21,9 @@ public class HomeWork28 {
 
             try {
                 System.out.println("Enter your string: ");
-                str = sc.nextLine();
+                str = sc.nextLine() +"\n";
                 Files.write(path, str.getBytes(), StandardOpenOption.APPEND);
+                System.out.println();
             } catch (IOException e) {
                 System.out.println("Mistake to write in to the file");
             }
