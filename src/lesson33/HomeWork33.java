012a5c98b659bfcdd6fbf834ef631dcb030aba90
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-05-07 12:47:55 +0300

    33 task

--- src/lesson33/HomeWork33.java
+++ src/lesson33/HomeWork33.java
@@ -1,7 +1,5 @@
 package lesson33;
 
-import java.util.Random;
-
 public class HomeWork33 {
     public static void main(String[] args) {
 
@@ -9,24 +7,5 @@ public class HomeWork33 {
         Thread thread1 = new Thread(tr);
         thread1.start();
 
-        int a;
-        try {
-            while (true) {
-                a = new Random().nextInt(1, 11);
-                checkNumber(a);
-                thread1.sleep(200);
-                System.out.println(a);
-            }
-        } catch (RuntimeException | InterruptedException e) {
-            throw new RuntimeException();
-        } catch (MyRandomException e) {
-            System.out.println("Your number is 5!");
-        }
     }
-    private static void checkNumber ( int a) throws MyRandomException {
-        if (a == 5) {
-            throw new MyRandomException();
-        }
-    }
-
 }
--- src/lesson33/MyRandomException.java
+++
@@ -1,7 +0,0 @@
-package lesson33;
-
-public class MyRandomException extends Exception {
-    public MyRandomException() {
-
-    }
-}
