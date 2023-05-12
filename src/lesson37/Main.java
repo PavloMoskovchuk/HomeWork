3c523b4b54b50c51f38122b1115e6909bd4dc0f9
PavloMoskovchuk <122542875+PavloMoskovchuk@users.noreply.github.com>
2023-05-12 19:07:49 +0300

    37 Home task

--- src/lesson37/Main.java
+++ src/lesson37/Main.java
@@ -15,3 +15,35 @@ public class Main {
         }
     }
 }
+
+
+//    CREATE SCHEMA HomeWork37;
+//        USE HomeWork37;
+//
+//        CREATE TABLE country(
+//        Country_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+//        Country VARCHAR(50),
+//        Country_Code VARCHAR(3)
+//        );
+//
+//        insert into country (Country, Country_Code) values ('Great Britain', 'GBR');
+//        insert into country (Country, Country_Code) values ('Ukraine', 'UKR');
+//        insert into country (Country, Country_Code) values ('Georgia', 'GEO');
+//
+//
+//        CREATE TABLE city(
+//        City_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+//        City VARCHAR(50),
+//        Country_Code VARCHAR(3)
+//        );
+//
+//        insert into city (City, Country_Code) values ('Tbilisi', 'GEO');
+//        insert into city (City, Country_Code) values ('Batumi', 'GEO');
+//        insert into city (City, Country_Code) values ('Kiyv', 'UKR');
+//        insert into city (City, Country_Code) values ('Lugansk', 'UKR');
+//        insert into city (City, Country_Code) values ('Lviv', 'UKR');
+//        insert into city (City, Country_Code) values ('London', 'GBR');
+//        insert into city (City, Country_Code) values ('Birmingham', 'GBR');
+//        insert into city (City, Country_Code) values ('Blacpool', 'GBR');
+//
+////        select * from country left join city on country.Country_Code = city.Country_Code
